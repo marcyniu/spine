c71cf62dc653e26667269d12042fc5da2254f9d8
Lance Rushing <lance.rushing@uptracs.com>
2016-01-15 10:00:36 -0800

    Finish inject with just constructor args

--- test/Container2Test.php
+++ test/Container2Test.php
@@ -8,6 +8,23 @@ class Container2Test_Test1
 
 }
 
+abstract class Container2Test_TestBase
+{
+}
+
+interface Container2Test_Interface
+{
+}
+
+class Container2Test_TestExtends extends  Container2Test_TestBase implements Container2Test_Interface{
+
+}
+class Container2Test_TestExtendsMore extends  Container2Test_TestExtends {
+
+}
+
+
+
 class Container2Test_Test2
 {
     /**
@@ -95,5 +112,96 @@ class Container2Test extends PHPUnit_Framework_TestCase
         $obj = new Container2Test_CallMethod();
 
         $this->container->callMethod($obj, 'testMethod');
+        $this->assertInstanceOf('Spine\\Container2Test_Test2', $obj->test2);
     }
+
+    public function testRegisterWithParents()
+    {
+        $newObject = new Container2Test_TestExtends();
+        $this->container->register($newObject);
+
+        $returned = $this->container->resolve("Spine\\Container2Test_TestExtends");
+        $this->assertEquals($newObject, $returned);
+
+
+        $interfaceProvider = $this->container->resolve("Spine\\Container2Test_Interface");
+        $this->assertEquals($interfaceProvider, $returned);
+
+        $baseInstance = $this->container->resolve("Spine\\Container2Test_TestBase");
+        $this->assertSame($returned, $baseInstance);
+
+
+    }
+
+
+    public function testRegisterWithOutParents()
+    {
+        $newObject = new Container2Test_TestExtends();
+        $this->container->register($newObject);
+
+        $returned = $this->container->resolve("Spine\\Container2Test_TestExtends");
+        $this->assertEquals($newObject, $returned);
+
+
+        $baseInstance = $this->container->resolve("Spine\\Container2Test_TestBase");
+        $this->assertSame($returned, $baseInstance);
+
+
+        $newerObject = new Container2Test_TestExtendsMore();
+        $this->container->register($newerObject, false);
+
+        $newerResolved = $this->container->resolve("Spine\\Container2Test_TestExtendsMore");
+        $this->assertNotSame($baseInstance, $newerResolved);
+
+
+    }
+
+    public function testRegisterTypeNoParents()
+    {
+        $newObject = new Container2Test_TestExtends();
+
+        $this->container->registerType("Spine\\Container2Test_TestBase", $newObject);
+
+        $returned = $this->container->resolve("Spine\\Container2Test_TestExtends");
+        $this->assertNotSame($newObject, $returned);
+
+
+        $baseInstance = $this->container->resolve("Spine\\Container2Test_TestBase");
+        $this->assertSame($newObject, $baseInstance);
+    }
+
+
+    public function testRegisterTypeWithParents()
+    {
+        $newObject = new Container2Test_TestExtendsMore();
+
+        $this->container->registerType("Spine\\Container2Test_TestExtends", $newObject, true);
+
+        $interfaceProvider = $this->container->resolve("Spine\\Container2Test_Interface");
+        $this->assertSame($newObject, $interfaceProvider);
+
+    }
+
+
+    public function testFactoryWithParents()
+    {
+        $factory = function () {
+            $obj              = new Container2Test_TestExtends();
+            return $obj;
+        };
+
+        $this->container->registerTypeFactory("Spine\\Container2Test_TestExtends", $factory, true);
+
+        $object = $this->container->resolve('Spine\\Container2Test_TestExtends');
+        $this->assertInstanceOf('Spine\\Container2Test_TestExtends', $object);
+
+
+        $baseObject = $this->container->resolve('Spine\\Container2Test_TestBase');
+        $this->assertSame($object, $baseObject);
+    }
+
+
+
+
+
 }
