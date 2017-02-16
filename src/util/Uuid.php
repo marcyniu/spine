b57efe5bb252e60a1099dbe34aa6961da1e83c53
Lance Rushing <lancerushing@gmail.com>
2017-02-15 20:59:19 -0800

    Use random_bytes() instead of openssl_random_pseudo_bytes()

--- src/util/Uuid.php
+++ src/util/Uuid.php
@@ -17,7 +17,7 @@ class UUID
      */
     public static function v4()
     {
-        $data = openssl_random_pseudo_bytes(16);
+        $data = random_bytes(16);
 
         $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
         $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
