9b5d1d4345b930aa5508103157eb8aa07153094b
Lance Rushing <lance.rushing@uptracs.com>
2018-03-08 13:50:39 -0800

    Only clean if the value is a string

--- src/web/Request.php
+++ src/web/Request.php
@@ -229,34 +229,36 @@ class Request
 
     protected function cleanParam(&$val)
     {
-        $val = trim($val);
-        $val = str_replace(
-            array(
-                "\xe2\x80\x98",
-                "\xe2\x80\x99",
-                "\xe2\x80\x9c",
-                "\xe2\x80\x9d",
-                "\xe2\x80\x93",
-                "\xe2\x80\x94",
-                "\xe2\x80\xa6"
-            ),
-            array(
-                "'",
-                "'",
-                '"',
-                '"',
-                '-',
-                '--',
-                '...'
-            ),
-            $val
-        );
-        // Next, replace their Windows-1252 equivalents.
-        $val = str_replace(
-            array(chr(145), chr(146), chr(147), chr(148), chr(150), chr(151), chr(133)),
-            array("'", "'", '"', '"', '-', '--', '...'),
-            $val
-        );
+        if (is_string($val)) {
+            $val = trim($val);
+            $val = str_replace(
+                array(
+                    "\xe2\x80\x98",
+                    "\xe2\x80\x99",
+                    "\xe2\x80\x9c",
+                    "\xe2\x80\x9d",
+                    "\xe2\x80\x93",
+                    "\xe2\x80\x94",
+                    "\xe2\x80\xa6"
+                ),
+                array(
+                    "'",
+                    "'",
+                    '"',
+                    '"',
+                    '-',
+                    '--',
+                    '...'
+                ),
+                $val
+            );
+            // Next, replace their Windows-1252 equivalents.
+            $val = str_replace(
+                array(chr(145), chr(146), chr(147), chr(148), chr(150), chr(151), chr(133)),
+                array("'", "'", '"', '"', '-', '--', '...'),
+                $val
+            );
+        }
     }
 
     /**
